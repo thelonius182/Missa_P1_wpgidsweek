7a4573f1b2ba79c356f7733d3cc99ec27dc7a43c
thelonius182 <lcavdakker@gmail.com>
2019-12-22 00:22:54 +0100

    refactor write.csv

--- src/wp_gidsweek.R
+++ src/wp_gidsweek.R
@@ -6,11 +6,16 @@ giva_latest_home <- "C:/cz_salsa/config/giva_start.txt"
 giva_latest <-
   read.csv(giva_latest_home,
            sep = "",
-           stringsAsFactors = FALSE) %>% 
+           stringsAsFactors = FALSE) %>%
   mutate(latest_run = ymd(latest_run))
 
 current_run <- giva_latest$latest_run + days(7)
 
 giva_latest %<>% mutate(latest_run = current_run)
 
-write.csv(x = giva_latest, file = giva_latest_home)
+write.csv2(
+  x = giva_latest,
+  file = giva_latest_home,
+  quote = F,
+  row.names = F
+)
